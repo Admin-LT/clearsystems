60a1227f30fbf22490c38ec2b01ab6df97d93c6b
Martynas Sklizmantas <saint@ghost.lt>
2025-04-14 13:23:04 +0200

    comments

---
+++ knyga/src/theme/DocItem/Footer/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Footer from '@theme-original/DocItem/Footer';
+import Comments from '@site/src/components/Comments';
+
+export default function FooterWrapper(props) {
+  return (
+    <>
+      <Footer {...props} />
+      <Comments />
+    </>
+  );
+}
+
